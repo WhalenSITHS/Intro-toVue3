4955017bbfab3e1c4555acb151fb9465805a12a4
mr. whalen <michael.whalen@sitechhs.com>
2023-03-02 09:55:27 -0500

    dfgh

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 v-if="graduated">{{ student }}</h1>
+  <h1 v-else>"They have not yet graduated"</h1>
+  <h1>{{ student }}</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      student: "Jason",
+      animals: ["pig", "cow", "ostrich", "Zee"],
+      graduated: true,
+    };
+  },
+};
+</script>
