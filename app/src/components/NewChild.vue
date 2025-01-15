a94c0e170d1e28e77d287a3062e8614e8e1b7315
mr. whalen <michael.whalen@sitechhs.com>
2025-01-15 07:49:27 -0500

    sdfg

---
+++ app/src/components/NewChild.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input v-model="name" type="text" />
+    <button @click="updateName">Click To Submit</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const name = ref('')
+const emit = defineEmits(['submitName'])
+const updateName = function () {
+  emit('submitName', name.value)
+}
+</script>
+
+<style lang="scss" scoped></style>
