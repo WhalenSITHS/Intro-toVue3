db889d002ef86c17a503b27b57de1285841f443b
mr. whalen <michael.whalen@sitechhs.com>
2025-01-30 08:00:05 -0500

    props doen

---
+++ app/src/components/LogIn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot></slot>
+    <h1>{{ title }}</h1>
+    <p>Count: {{ count }}</p>
+    <p v-if="isActive">This is active!</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  count: Number,
+  isActive: Boolean,
+})
+</script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <LogIn title="Hello Vue" :count="5" :isActive="true"><h1>Using Slots!</h1></LogIn>
+  <div class="container">
+    <AnimalCard v-for="animal in animals" :key="animal.name" :animal />
+  </div>
 </template>
+
+<script setup>
+import LogIn from '../components/LogIn.vue'
+import AnimalCard from '../components/AnimalCard.vue'
+const animals = [
+  { name: 'Walrus', image: '/walrus.webp' },
+  { name: 'Capybara', image: '/capy.jpg' },
+]
+</script>
